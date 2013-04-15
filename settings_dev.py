3c1b0ce99967b538c8c339f6406f86cb3f5386c0
Joey Rhyu <joeyrhyu@gmail.com>
2013-04-15 00:12:54 -0400

    update redirect path

--- settings_dev.py
+++ settings_dev.py
@@ -1,6 +1,6 @@
 facebook_api_key = '262869930403008'
 facebook_secret = '9dc16baa1f8dc2aa1710491019a49c6f'
 cookie_secret = '2T20sljETVSq4F5UGd2mdXBtB17phE/BmhEisB+qW9A='
-redirect_path = '/auth/login'
+redirect_path = 'http://thawing-retreat-1257.herokuapp.com/auth/login'
 #redirect_path = 'http://localhost:8000/auth/login'
 scope = 'read_friendlists,publish_actions,user_online_presence,read_stream,user_photos'
