44197ac0fa80cb78ab2dcfcee9cad77621ff4a09
Joey Rhyu <joeyrhyu@gmail.com>
2013-04-15 00:03:45 -0400

    added fb auth functionality and simple templates

--- app.py
+++ app.py
@@ -16,16 +16,75 @@ define("port", default=8000, type=int)
 define("facebook_api_key")
 define("facebook_secret")
 define("cookie_secret")
+define("redirect_path")
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler, tornado.auth.FacebookGraphMixin):
+    @tornado.web.asynchronous
     def get(self):
-        self.write("Hi")
+        access_token = self.get_secure_cookie('access_token')
+        if not access_token: 
+            print "don't have access token"
+            self.redirect('/auth/login')
+            return
+
+        self.facebook_request(
+            "/me/feed",
+            access_token=access_token,
+            callback=self.async_callback(self._on_facebook_user_feed))
+
+    def _on_facebook_user_feed(self, response):
+        name=self.get_secure_cookie('user_name')
+        print "in user feed" + name
+        print response
+        self.render('home.html', feed=response['data'] if response else [], name=name)
+
+class LoginHandler(tornado.web.RequestHandler, tornado.auth.FacebookGraphMixin):
+    @tornado.web.asynchronous
+    def get(self):
+        uid = self.get_secure_cookie('user_id')
+
+        # check whether the FB token is passed in the query string
+        if self.get_argument('code', None):
+            self.get_authenticated_user(
+                redirect_uri=self.settings['redirect_path'],
+                client_id=self.settings['facebook_api_key'],
+                client_secret=self.settings['facebook_secret'],
+                code=self.get_argument('code'),
+                callback=self.async_callback(self._on_facebook_login))
+            return
+        # check whether the person has a cookie
+        elif self.get_secure_cookie('access_token'):
+            self.redirect('/')
+            return
+
+        self.authorize_redirect(
+            redirect_uri=self.settings['redirect_path'],
+            client_id=self.settings['facebook_api_key'],
+            extra_params={'scope': 'read_stream,publish_stream'}
+        )
+
+    def _on_facebook_login(self, user):    
+        if not user:
+            self.clear_all_cookies()
+            raise tornado.web.HTTPError(500, 'Facebook authentication failed')
+
+        self.set_secure_cookie('user_id', str(user['id']))
+        self.set_secure_cookie('user_name', str(user['name']))
+        self.set_secure_cookie('access_token', str(user['access_token']))
+        self.redirect('/')
+
+class LogoutHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.clear_all_cookies()
+        self.render('logout.html')
 
 class Application(tornado.web.Application): 
     def __init__(self):
         dir_name = os.path.dirname(__file__)
         handlers = [
-            (r'/', MainHandler)
+            (r'/', MainHandler),
+            (r'/auth/login', LoginHandler),
+            (r'/auth/logout', LogoutHandler)
         ]
         settings = {
             "template_path": os.path.join(dir_name, "templates"),
@@ -33,7 +92,8 @@ class Application(tornado.web.Application):
             "facebook_api_key": options.facebook_api_key,
             "facebook_secret": options.facebook_secret,
             "cookie_secret": options.cookie_secret,
-            "xsrf_cookies": True
+            "xsrf_cookies": True,
+            "redirect_path": options.redirect_path
         }
         super(Application, self).__init__(handlers, **settings)
 
